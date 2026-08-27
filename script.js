befa446ff9f8911f564dd5bdf72a3a2ba10679cf
maithe heloise <maithe.faria@escola.pr.gov.br>
2026-08-27 07:35:49 -0300

    salvar

--- script.js
+++ script.js
@@ -6,11 +6,17 @@ const textoResultado = document.querySelector(".texto-resultado");
 
 const perguntas = [
     {
-    enunciado: "Pergunta 1", 
-    alternativas: [
-        "Alternativa 1",
-        "Alternativa 2"
-         ]
-    }   
-    "pergunta 2"
-]
+        enunciado: "Pergunta 1",
+        alternativas: [
+            "Alternativa 1",
+            "Alternativa 2"
+        ]
+    },
+    {
+        enunciado: "Pergunta 2",
+        alternativas: [
+            "Alternativa 1",
+            "Alternativa 2"
+        ]
+    }
+]; 
